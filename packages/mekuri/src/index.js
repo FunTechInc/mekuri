3c7197cd2b461cceb12533437ddf52d4828b0774
takuma-hmng8 <t.hashimoto@funtech-fpro.com>
2023-09-09 17:12:27 +0900

    v3.1.8

--- packages/mekuri/src/index.js
+++ packages/mekuri/src/index.js
@@ -1,4 +1,5 @@
 export { MekuriContext, useMekuriDuration } from "./context/MekuriContext";
 export { useMekuriAnimation } from "./hook/useMekuriAnimation";
+export { intersectionObserverHandler } from "./hook/utils/intersectionObserverHandler";
 export { MekuriFreezer } from "./component/MekuriFreezer";
 export { Mekuri } from "./component/Mekuri";
